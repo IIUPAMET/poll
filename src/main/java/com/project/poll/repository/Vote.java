a2f7c4e8d1f6a10d2550cbaf99cf4b23f80ec78f
Mykola Filimonov <mykola.filimonov@thomsonreuters.com>
2020-01-17 14:11:14 +0200

    Added poll entities, also hibernate database initialization

---
+++ src/main/java/com/project/poll/repository/Vote.java
@@ -0,0 +1,25 @@
+package com.project.poll.repository;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "vote")
+public class Vote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "userId")
+    private int userId;
+
+    @Column(name = "optionId")
+    private int optionId;
+
+}
